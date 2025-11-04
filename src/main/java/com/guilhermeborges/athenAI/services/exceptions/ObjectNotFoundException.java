e889670a3d4ece516b3f8f3f1da42ae42f023ade
GuiPrado275 <guilherme.silva645@fatec.sp.gov.br>
2025-11-04 16:04:40 -0300

    Create Authorization and ObjectNotFoun Exception

--- src/main/java/com/guilhermeborges/athenAI/services/exceptions/ObjectNotFoundException.java
+++ src/main/java/com/guilhermeborges/athenAI/services/exceptions/ObjectNotFoundException.java
@@ -1,4 +1,15 @@
 package com.guilhermeborges.athenAI.services.exceptions;
 
-public class ObjectNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND) //the errors of TaskService and UserService are 404 now
+public class ObjectNotFoundException  extends EntityNotFoundException {
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+
 }
