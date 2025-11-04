0c0ed317e2120551b42fbad06ccae172bcd01415
GuiPrado275 <guilherme.silva645@fatec.sp.gov.br>
2025-11-04 04:21:08 -0300

    athenAI

---
+++ src/main/java/com/guilhermeborges/athenAI/AthenAiApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeborges.athenAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilhermeborges/athenAI/AthenAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermeborges.athenAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
